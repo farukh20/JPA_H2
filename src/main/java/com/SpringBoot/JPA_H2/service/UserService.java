41b20b60405e8d89ae9f36bd78d14207cd7fac3b
Farukh <xfarukh.3@gmail.com>
2025-11-29 23:23:25 +0530

    UserService added {getAllUser,saveUser}

---
+++ src/main/java/com/SpringBoot/JPA_H2/service/UserService.java
@@ -0,0 +1,24 @@
+package com.SpringBoot.JPA_H2.service;
+
+import com.SpringBoot.JPA_H2.entity.UserEntity;
+import com.SpringBoot.JPA_H2.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    public UserService(UserRepository userRepository){
+        this.userRepository=userRepository;
+    }
+
+    public List<UserEntity> getAllUser(){
+        return userRepository.findAll();
+    }
+
+    public void saveUser(@RequestBody UserEntity userEntity){
+        userRepository.save(userEntity);
+    }
+}
