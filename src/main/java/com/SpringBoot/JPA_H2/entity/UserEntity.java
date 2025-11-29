238efa3ced112462ce5e1fde7e1bed70be70f6f0
Farukh <xfarukh.3@gmail.com>
2025-11-29 23:01:49 +0530

    UserEntity added {id,name,email,number}

---
+++ src/main/java/com/SpringBoot/JPA_H2/entity/UserEntity.java
@@ -0,0 +1,33 @@
+package com.SpringBoot.JPA_H2.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+
+@Entity
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull(message = "name cannot be null")
+    private String name;
+
+    @NotNull(message = "Email cannot be null")
+    @Email(message = "Invalid email format")
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotNull(message = "number cannot be null")
+    @Pattern(regexp = "^[0-9]{10}$" , message = "number should be exactly 10 digits")
+    private String number;
+}
